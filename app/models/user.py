027a9c4b0cc1e2ade76af7bfcba9006d13ef57ba
AzeemAhsanM <azeemahsan67@gmail.com>
2025-07-11 06:12:56 +0530

    some corrections

--- app/models/user.py
+++ app/models/user.py
@@ -1,15 +1,15 @@
-from sqlalchemy import Boolean, column, Integer, ForeignKey, String
+from sqlalchemy import Boolean, Column, Integer, ForeignKey, String
 from db.database import Base
 
 class User(Base):
     __tablename__ = "users"
 
-    id = column(Integer, primary_key=True, index=True)
-    username = column(String, unique=True, index=True)
-    email = column(String, unique=True, index=True)
-    hashed_password = column(String)
-    is_active = column(Boolean, default=True)
-    is_superuser = column(Boolean, default=False)
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    email = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+    is_active = Column(Boolean, default=True)
+    is_superuser = Column(Boolean, default=False)
 
     def __repr__(self):
         return f"<User(id={self.id}, username={self.username}, email={self.email})>"
@@ -18,11 +18,11 @@ class User(Base):
 class foodEntry(Base):
     __tablename__ = "food_entries"
 
-    id = column(Integer, primary_key=True, index=True)  # Primary key for food entries
-    user_id = column(Integer, ForeignKey("users.id"), nullable=False)  # Foreign key to users table
-    food_name = column(String, index=True)
-    calories = column(Integer, nullable=False)
-    date = column(String, nullable=False)  # Store date as a string (e.g., "YYYY-MM-DD")
+    id = Column(Integer, primary_key=True, index=True)  # Primary key for food entries
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)  # Foreign key to users table
+    food_name = Column(String, index=True)
+    calories = Column(Integer, nullable=False)
+    date = Column(String, nullable=False)  # Store date as a string (e.g., "YYYY-MM-DD")
 
     def __repr__(self):
         return f"<FoodEntry(id={self.id}, user_id={self.user_id}, food_name={self.food_name}, calories={self.calories}, date={self.date})>"    
